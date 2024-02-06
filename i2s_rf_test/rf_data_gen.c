557434817c20d4009700994e03f5ef7636793fb9
cnlohr <lohr85@gmail.com>
2024-02-06 04:25:05 -0800

    Last bit of tidying up for now

--- i2s_rf_test/rf_data_gen.c
+++ i2s_rf_test/rf_data_gen.c
@@ -5,13 +5,13 @@
 
 int main()
 {
-	FILE * f = fopen( "chirpbuff.h", "w" );
+	//FILE * f = fopen( "chirpbuff.h", "w" );
 	FILE * fd = fopen( "chirpbuff.dat", "w" );
 	double chirp_begin = 903.9-.075;
 	double chirp_end = 903.9+.075;
 	double sample_rate = 1040.0/6.0; // Sampler at 173MHz.
 	double sampletotal = 8192*(32);
-	fprintf( f, "const uint32_t chirpbuff[%d] = {\n", (int)(sampletotal/32) );
+	//fprintf( f, "const uint32_t chirpbuff[%d] = {\n", (int)(sampletotal/32) );
 	int samples;
 
 	// For a given word, it is shifted out MSB (Bit and byte) first
@@ -29,7 +29,7 @@ int main()
 		samplect++;
 		if( samplect == 32 )
 		{
-			fprintf( f, "0x%08x,%c", sample_word, (words & 0xf0)?' ':'\n' );
+			//fprintf( f, "0x%08x,%c", sample_word, (words & 0xf0)?' ':'\n' );
 			fwrite( &sample_word, 1, 4, fd );
 			words++;
 			sample_word = 0;
@@ -38,8 +38,8 @@ int main()
 		sample_word <<= 1;
 	}
 
-	fprintf( f, "};\n" );
-	fclose( f );
+	//fprintf( f, "};\n" );
+	//fclose( f );
 	fclose( fd );
 	fprintf( stderr, "Wrote out %d uint32_t's.\n", words );
 }
